73da0cecd72165a5f3792f14fca59ce7f79a5e10
Joshua Smith <mtgmirage@protonmail.com>
2024-11-20 18:42:25 -0600

    Added user menu using while loop -- option 0 exists, option 1 displays list of plant species names, options 2, 3, and 4 throw a NotImplementedException, and any other input is invalid

--- Program.cs
+++ Program.cs
@@ -51,7 +51,44 @@ List<Plant> plants = new List<Plant>()
 Console.WriteLine(@"Welcome to ExtraVert FlowerShop
 AKA that one place");
 
-ListPlants();
+// User Menu
+string choice = null;
+while (choice != "0")
+{
+    Console.WriteLine(@"Choose an option:
+        0. Exit
+        1. Display all plants
+        2. Post a plant to be adopted
+        3. Adopt a plant
+        4. Delist a plant"
+    );
+
+    choice = Console.ReadLine();
+    if (choice == "0")
+    {
+        Console.WriteLine("Goodbye!");
+    }
+    else if (choice == "1")
+    {
+        ListPlants();
+    }
+    else if (choice == "2")
+    {
+        throw new NotImplementedException("Post a plant to be adopted");
+    }
+    else if (choice == "3")
+    {
+        throw new NotImplementedException("Adopt a plant");
+    }
+    else if (choice == "4")
+    {
+        throw new NotImplementedException("Delist a plant");
+    }
+    else 
+    {
+        Console.WriteLine("Invalid Input!");
+    }
+}   
 
 void ListPlants()
 {
